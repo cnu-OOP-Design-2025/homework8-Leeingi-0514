f724398dd5643540bdfa4cb7e02497a5e2ef86fc
LEE <lee@LEE-MacBookPro.local>
2025-11-11 14:17:21 +0900

    과제제출

--- game.cpp
+++ game.cpp
@@ -12,4 +12,10 @@ int Archer::getAttack() const {return 60;}
 int Archer::getSpeed() const {return 70;}
 int Archer::getDefense() const {return 40;}
 
-int Armor::getAttack() const {}
+int Staff::getAttack() const {return character->getAttack() + 20;}
+int Sword::getAttack() const {return character->getAttack() + 30;}
+int Bow::getAttack() const {return character->getAttack() + 25;}
+int Boots::getSpeed() const {return character->getSpeed() + 15;}
+int Armor::getDefense() const {return character->getDefense() + 30;}
+int Armor::getSpeed() const {return character->getSpeed() - 5;}
+
